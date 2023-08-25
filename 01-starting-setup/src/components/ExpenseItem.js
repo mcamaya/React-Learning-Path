c1ed06ca7e6262240efabba53e97a0700b0bc47c
mcamaya <camiamayar@gmail.com>
2023-08-24 23:35:50 -0500

    feat: :sparkles: Logic in components - Modifying Date Objects

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,13 +1,20 @@
 import './ExpenseItem.css';
 
-function ExpenseItem(props) {
+function ExpenseItem({title, date, amount}) {
+  const month = date.toLocaleString('en-US', {month: 'long'});
+  const day = date.toLocaleString('en-US', {day: '2-digit'});
+  const year = date.getFullYear();
 
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
       </div>
     </div>
   );
