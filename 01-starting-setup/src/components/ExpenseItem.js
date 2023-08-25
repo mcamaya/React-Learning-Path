dd1965d80c7ed5609a352ce85f8eef0a572cd9c0
mcamaya <camiamayar@gmail.com>
2023-08-25 00:37:03 -0500

    feat :sparkles: Spliting components, inserting one into another

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,20 +1,13 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
-function ExpenseItem({title, date, amount}) {
-  const month = date.toLocaleString('en-US', {month: 'long'});
-  const day = date.toLocaleString('en-US', {day: '2-digit'});
-  const year = date.getFullYear();
-
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
-        <div className="expense-item__price">${amount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
