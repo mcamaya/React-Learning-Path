0396ab121f659ba77c44dbfb3f061499118e72d4
mcamaya <camiamayar@gmail.com>
2023-09-21 07:33:11 -0500

    feat: :sparkles: Escuchando eventos de clic e input

--- src/components/ToDoSearch.js
+++ src/components/ToDoSearch.js
@@ -1,9 +1,21 @@
+import React from "react";
 import "./ToDoSearch.css";
+
 function ToDoSearch() {
+	const [searchValue, setSearchValue] = React.useState('');
+
 	return (
 		<div class="coolinput">
 			<label for="input" class="text">Buscar ToDo:</label>
-			<input type="text" placeholder="Llorar con la llorona..." name="input" class="input" />
+			<input 
+				type="text" 
+				placeholder="Llorar con la llorona..." 
+				class="input" 
+				value={searchValue}
+				onChange={(event) => {
+					setSearchValue(event.target.value);
+				}}
+			/>
 		</div>
 	);
 }
